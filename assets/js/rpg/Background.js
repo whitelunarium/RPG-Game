cf7ee0d67c8d187e5513f7fa5afaaf0c7895c2ea
VeeraKalakota <85570844+VeeraKalakota@users.noreply.github.com>
2024-10-19 15:33:28 -0700

    Restore Background.js

--- assets/js/rpg/Background.js
+++ assets/js/rpg/Background.js
@@ -1,43 +1,41 @@
+
 import GameEnv from './GameEnv.js';
 
-class Background {
-    constructor(images = []) {
-        this.images = images.map(src => {
-            const img = new Image();
-            img.src = src;
-            return img;
-        });
-        this.currentImageIndex = 0;
-        this.loadImages();
-    }
+export class Background {
+    constructor(imageSrc = null, imageSrc2 = null) {
+        if (imageSrc) {
+            this.image = new Image();
+            this.image.src = imageSrc.src;
+        } else {
+            this.image = null;
+        }
 
-    loadImages() {
-        const promises = this.images.map(img => {
-            return new Promise((resolve, reject) => {
-                img.onload = resolve;
-                img.onerror = reject;
-            });
-        });
-
-        Promise.all(promises).then(() => {
-            console.log('All background images loaded successfully.');
-        }).catch(() => {
-            console.error('Failed to load some background images.');
-        });
-    }
-
-    draw() {
-        const currentImage = this.images[this.currentImageIndex];
-        if (currentImage) {
-            GameEnv.ctx.drawImage(
-                currentImage,
-                0, 0, GameEnv.innerWidth, GameEnv.innerHeight
-            );
+        if (imageSrc2) {
+            this.image2 = new Image();
+            this.image2.src = imageSrc2.src;
+        } else {
+            this.image2 = null;
         }
     }
 
-    changeBackground() {
-        this.currentImageIndex = (this.currentImageIndex + 1) % this.images.length;
+    draw() {
+        const ctx = GameEnv.ctx;
+        const width = GameEnv.innerWidth;
+        const height = GameEnv.innerHeight;
+
+        // Draw the first background image if it exists
+        if (this.image) {
+            ctx.drawImage(this.image, 0, 0, width, height);
+        } else {
+            // Fill the canvas with a default color if no first background image is provided
+            ctx.fillStyle = '#87CEEB';
+            ctx.fillRect(0, 0, width, height);
+        }
+
+        // Draw the second stationary background image if it exists
+        if (this.image2) {
+            ctx.drawImage(this.image2, 0, 0, width, height);
+        }
     }
 }
 
